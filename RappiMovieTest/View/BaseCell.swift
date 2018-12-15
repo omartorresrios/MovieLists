2adefd34ce53757e1c4f812bb4f3056d00241890
Omar Torres <torresomar44@gmail.com>
2018-12-14 19:56:39 -0500

    Add BaseCell

---
+++ RappiMovieTest/View/BaseCell.swift
@@ -0,0 +1,24 @@
+//
+//  BaseCell.swift
+//  RappiMovieTest
+//
+//  Created by Omar Torres on 12/12/18.
+//  Copyright © 2018 OmarTorres. All rights reserved.
+//
+
+import UIKit
+
+class BaseCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    func setupViews() {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
